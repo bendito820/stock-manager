95b29269d0f606c36e9ecde04234ac98369c1613
bendito820 <bendito820@gmail.com>
2024-05-31 12:05:28 +0100

    Create add new product page

---
+++ app/products/new/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/components/header";
+import React from "react";
+
+const NewProductPage = () => {
+  return (
+    <div className="mt-6">
+      <Header>Adicionar novo produto</Header>
+    </div>
+  );
+};
+
+export default NewProductPage;

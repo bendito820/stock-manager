ec10fbfa37d80ae1022b8a8ee785cd816f89c53d
bendito820 <bendito820@gmail.com>
2024-05-31 11:34:08 +0100

    Implement products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className="mt-6">
+      <div className="space-y-6 max-w-xl">
+        <h1 className="text-4xl font-semibold text-zinc-800">Produtos</h1>
+        <p className="text-zinc-500">
+          Todos os produtos que a empresa possui armazenados. Tanto as
+          matérias-primas como os produtos acabados
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsPage;

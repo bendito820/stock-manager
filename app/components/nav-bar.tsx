7dd2c0f8134a37c4d3aefd0da61b404c4f82c16f
Antonio Costa <bendito820@gmail.com>
2024-06-01 12:12:41 +0100

    Fix container issues

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -3,9 +3,11 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="container mx-auto px-4 bg-emerald-800 text-white h-16 flex justify-between items-center">
-      <h2 className="font-bold">STOCK</h2>
-      <Link href={"/products"}></Link>
+    <nav className="px-4 bg-emerald-800 text-white h-16 flex justify-between items-center">
+      <div className="container mx-auto max-w-6xl ">
+        <h2 className="font-bold">STOCK</h2>
+        <Link href={"/products"}></Link>
+      </div>
     </nav>
   );
 };

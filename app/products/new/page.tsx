e829e389707e03970e07377dd0d5d541ad5a4ff7
Antonio Costa <bendito820@gmail.com>
2024-06-01 22:08:42 +0100

    Implement form component and refactor

--- app/products/new/page.tsx
+++ app/products/new/page.tsx
@@ -1,19 +1,18 @@
+import AddNewProductForm from "@/app/components/add-new-product-form";
 import Header from "@/app/components/header";
 import Text from "@/app/components/text";
-import React from "react";
 
-const NewProductPage = () => {
+export default function NewProductPage() {
   return (
     <div className="mt-6">
       <div className="space-y-6 max-w-xl">
         <Header>Novo Produto</Header>
         <Text>Adicone novos produtos</Text>
-        <Text>
-          <span className="text-red-500">*</span> Representa campos obrigatorios
-        </Text>
+      </div>
+
+      <div className="mt-8">
+        <AddNewProductForm />
       </div>
     </div>
   );
-};
-
-export default NewProductPage;
+}

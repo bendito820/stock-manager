167635b5fa6998ad6ff3f5f88fd7b10a07d5db3d
Antonio Costa <bendito820@gmail.com>
2024-06-03 12:38:45 +0100

    Fix ProductNavBar responsivability issues

--- app/components/products-nav-bar.tsx
+++ app/components/products-nav-bar.tsx
@@ -18,21 +18,32 @@ const links: {
 export default function ProductsNavBar() {
   const currentPath = usePathname();
   return (
-    <div className="bg-zinc-300 p-2 flex flex-row justify-between rounded-md">
-      {links.map((link) => (
-        <button key={link.href} className="p-2 transition-colors duration-150">
-          <Link
-            className={`${
-              currentPath === link.href ? "font-bold" : ""
-            } text-emerald-800 hover:text-emerald-900 ease-in-out hover:font-semibold focus:text-success-600 active:text-emerald-900 text-sm text-decoration-line: underline`}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
-        </button>
-      ))}
+    <div className="space-y-2">
+      <div className="md:hidden">
+        <NewProductButton />
+      </div>
 
-      <NewProductButton />
+      <div className="bg-zinc-300 p-2 flex flex-row justify-between rounded-md">
+        {links.map((link) => (
+          <button
+            key={link.href}
+            className="p-2 transition-colors duration-150"
+          >
+            <Link
+              className={`${
+                currentPath === link.href ? "font-bold" : ""
+              } text-emerald-800 hover:text-emerald-900 ease-in-out hover:font-semibold focus:text-success-600 active:text-emerald-900 text-sm text-decoration-line: underline`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </button>
+        ))}
+
+        <div className="hidden md:block">
+          <NewProductButton />
+        </div>
+      </div>
     </div>
   );
 }

ff46bca612c79c3e045eac0dd51825465e638e9e
Antonio Costa <bendito820@gmail.com>
2024-06-03 19:34:23 +0100

    Implement delete button

--- app/components/GroupListComponent.tsx
+++ app/components/GroupListComponent.tsx
@@ -2,6 +2,8 @@
 
 import { client } from "@/app/api/client";
 import Spinner from "@/app/components/spinner";
+import { useState } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 import { useQuery } from "react-query";
 import Text from "./text";
 
@@ -9,6 +11,7 @@ export default function GroupListComponent() {
   const {
     isLoading,
     error,
+    refetch,
     data: groups,
   } = useQuery("groups", () =>
     client
@@ -22,12 +25,44 @@ export default function GroupListComponent() {
     <div className="space-y-2 max-w-xl rounded-sm">
       {groups?.map((group) => (
         <div
-          className="bg-zinc-300 p-2 cursor-pointer hover:font-semibold transition-colors duration-150"
+          className="bg-zinc-300 p-2 flex justify-between items-center cursor-pointer hover:font-semibold transition-colors duration-150 rounded-md"
           key={group._id}
         >
           <Text>{group.name}</Text>
+          <DeleleteGroupButton refetch={refetch} groupId={group._id} />
         </div>
       ))}
     </div>
   );
 }
+
+const DeleleteGroupButton = ({
+  groupId,
+  refetch,
+}: {
+  groupId: string;
+  refetch: any;
+}) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleDelete = async () => {
+    try {
+      setLoading(true);
+      await client.delete(`/groups/${groupId}`);
+      setLoading(false);
+      refetch();
+      // router.push("/products");
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+  return (
+    <button
+      disabled={loading}
+      onClick={() => handleDelete()}
+      className="p-2 bg-red-200 hover:bg-red-400 rounded-md"
+    >
+      {loading ? <Spinner /> : <FaRegTrashAlt />}
+    </button>
+  );
+};
--- app/components/spinner.tsx
+++ app/components/spinner.tsx
@@ -5,7 +5,7 @@ export default function Spinner() {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="inline w-10 h-5 text-gray-200 animate-spin dark:text-gray-200 fill-green-500"
+        className="inline w-5 h-4 text-gray-200 animate-spin dark:text-gray-200 fill-green-500"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"

3b555f21d6995e1cd0d1de911261260b5350d470
Antonio Costa <bendito820@gmail.com>
2024-06-04 12:29:00 +0100

    Update client url

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://192.168.0.18:3030/v1",
+  // baseURL: "http://192.168.0.18:3030/v1",
+  baseURL: "http://192.168.1.240:3030/v1",
   timeout: 1000,
   headers: { "X-Custom-Header": "foobar" },
 });
